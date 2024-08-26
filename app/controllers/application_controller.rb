03e2f88e43599c918107d30c0726a882e1e435ea
duongtnhat <duong.tuan.nhat.96.it@gmail.com>
2024-08-27 00:09:01 +0700

    remove cors header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  after_filter :cors_set_access_control_headers
+  # after_filter :cors_set_access_control_headers
 
   DEFAULT_PAGE_LIMIT = 5
   def success data, serializer
@@ -22,12 +22,10 @@ class ApplicationController < ActionController::API
     @current_user ||= current_devise_api_token&.resource_owner
   end
 
-  # For all responses in this controller, return the CORS access control headers.
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = '*'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = '*'
+  #   headers['Access-Control-Allow-Headers'] = '*'
+  #   headers['Access-Control-Max-Age'] = "1728000"
+  # end
 end
