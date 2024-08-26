3c759f2e62b047b35a9072f84ca345cb30160ac2
duongtnhat <duong.tuan.nhat.96.it@gmail.com>
2024-08-26 23:38:26 +0700

    fix cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  after_filter :cors_set_access_control_headers
+
   DEFAULT_PAGE_LIMIT = 5
   def success data, serializer
     data = serializer.new(data)
@@ -18,5 +20,14 @@ class ApplicationController < ActionController::API
 
   def current_user
     @current_user ||= current_devise_api_token&.resource_owner
+  end
+
+  # For all responses in this controller, return the CORS access control headers.
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = '*'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
 end
